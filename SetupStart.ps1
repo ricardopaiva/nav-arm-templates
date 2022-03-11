10dee3c307bbcf7618e0b9444ba385c133b79c3a
Ricardo Moinhos <ricardomo@lsretail.com>
2022-03-11 14:40:00 +0000

    Testing changes.

--- SetupStart.ps1
+++ SetupStart.ps1
@@ -66,7 +66,9 @@ elseif ($nchBranch -eq "") {
 
 if (-not (Get-InstalledModule Az -ErrorAction SilentlyContinue)) {
     AddToStatus "Installing Az module (this might take a while)"
-    Install-Module Az -Force
+    #Install-Module Az -Force
+    Install-Module Az.Compute -Force
+    Install-Module Az.Resources -Force
 }
 
 if (-not (Get-InstalledModule AzureAD -ErrorAction SilentlyContinue)) {
