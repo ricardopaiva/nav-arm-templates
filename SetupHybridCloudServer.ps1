7b314b865999cb6d68a26ca77241a0de2d765a17
Ricardo Moinhos <ricardomo@lsretail.com>
2022-03-23 18:46:54 +0000

    Testing changes.
    License file download from Azure Storage.

--- SetupHybridCloudServer.ps1
+++ SetupHybridCloudServer.ps1
@@ -5,10 +5,6 @@ if (!(Test-Path function:AddToStatus)) {
     }
 }
 
-AddToStatus "Who am i: $(whoami)"
-
-AddToStatus "1: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
-
 . (Join-Path $PSScriptRoot "settings.ps1")
 
 $Folder = "C:\DOWNLOAD\HybridCloudServerComponents"
@@ -23,24 +19,17 @@ if (!(Test-Path $Filename)) {
 
 AddToStatus "Installing GoCurrent Client module"
 . "$Filename" /VERYSILENT /NORESTART /SUPPRESSMSGBOXES | Out-Null
-#. "$Filename" /VERYSILENT /NORESTART /SUPPRESSMSGBOXES
-# Start-Sleep -s 5  # Waits 5 seconds to continue.
 if ($LASTEXITCODE -ne 0) { 
     AddToStatus -color red "Error installing GoCurrent Client module: $($LASTEXITCODE)"
     return
 }
-AddToStatus "2: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "2: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
 
 $env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
 Install-GocPackage -Id 'go-current-client'
-# $env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
+$env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
 
-AddToStatus "3: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "3: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
-
-# AddToStatus "Installing SQL Server Express (this might take a while)"
-# Install-GocPackage -Id 'sql-server-express'
+AddToStatus "Installing SQL Server Express (this might take a while)"
+Install-GocPackage -Id 'sql-server-express'
 
 # AddToStatus "Preparing SQL Server Studio Management (SSMS) installation (this might take a while)"
 # . "c:\demo\SetupSSMS.ps1"
@@ -56,32 +45,18 @@ $dd_config | % { $_.Replace("<WebSrv>false</WebSrv>", "<WebSrv>true</WebSrv>") }
 AddToStatus "Installing Update Service Server"
 Install-GocPackage -Id 'go-current-server'
 
-AddToStatus "4: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "4: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
-
-AddToStatus "4: $(Get-Module)"
-
 AddToStatus "Installing Update Service Server Management"
 Install-GocPackage -Id 'go-current-server-management'
 
-AddToStatus "5: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "5: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
-
 $env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
-
-AddToStatus "6: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "6: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
 
 Import-Module GoCurrent
 Import-Module GoCurrentServer
+
 $ServerAssembly = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName.StartsWith('LSRetail.GoCurrent.Server.Management')}
 $ClientAssembly = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName.StartsWith('LSRetail.GoCurrent.Client.Management')}
-AddToStatus "ServerAssembly: $($ServerAssembly)"
-AddToStatus "ClientAssembly: $($ClientAssembly)"
 $ServerVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ServerAssembly.Location).ProductVersion
 $ClientVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ClientAssembly.Location).ProductVersion
-AddToStatus "ServerVersion: $($ServerVersion)"
-AddToStatus "ClientVersion: $($ClientVersion)"
 if ($ServerVersion -ne $ClientVersion)
 {
     Write-Warning "Client and server version are not the same ($ServerVersion vs $ClientVersion)"
@@ -101,9 +76,6 @@ $Arguments = @{
 }
 Install-GocPackage -Id 'ls-central-hcc-project' -Arguments $Arguments
 
-# Refresh the PS Module Path otherwise we will get "The specified module 'LsPackageTools\LicensePackageCreator' was not loaded because no valid module file was found in any module directory." when creating the license package.
-# $env:PSModulePath = [System.Environment]::GetEnvironmentVariable('PSModulePath', 'Machine')
-
 AddToStatus "Installing Hybrid Cloud Components"
 Set-Location $HCCProjectDirectory
 
@@ -115,24 +87,17 @@ if ($licenseFileUri) {
     Copy-Item -Path $LicenseFileSourcePath -Destination $LicenseFileDestinationPath -Force
 }
 else {
-    # Install-Module Az.Storage -Force
-    AddToStatus "1"
     Import-Module Az.Storage
 
-    AddToStatus "2"
     $storageAccountName = 'storagerlkhmkieze3cg'
     $containerName = 'hcs-container'
     $licenseFileName = 'DEV.flf'
     
-    AddToStatus "3"
     $sasToken = '?sv=2020-08-04&ss=b&srt=o&se=2022-03-24T16%3A42%3A39Z&sp=rl&sig=fENFcm8hXng%2BJUF1QcCnWgzhOL6f%2FtqehEsEHF6ELmY%3D'
-    AddToStatus "4"
     $storageAccountContext = New-AzStorageContext $storageAccountName -SasToken $sasToken
-    AddToStatus "5"
 
     $LicenseFileSourcePath = "c:\demo\license.flf"
     $LicenseFileDestinationPath = (Join-Path $HCCProjectDirectory 'Files/License')
-    AddToStatus "6"
 
     $DownloadBCLicenseFileHT = @{
         Blob        = $licenseFileName
@@ -140,18 +105,13 @@ else {
         Destination = $LicenseFileSourcePath
         Context     = $storageAccountContext
     }
-    AddToStatus "7"
     Get-AzStorageBlobContent @DownloadBCLicenseFileHT
-    AddToStatus "8"
     Copy-Item -Path $LicenseFileSourcePath -Destination $LicenseFileDestinationPath -Force
-    AddToStatus "9"
 }
 # else {
 #     throw "License file not found at: ${licenseFileUri}"
 # }
 
-AddToStatus "7: env:PSModulePath: $($env:PSModulePath)"
-AddToStatus "7: $([System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine"))"
 $env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath", "Machine")
 
 AddToStatus "Creating license package"
