3759f8be836bc2d7a2107eef4c84d5640e4ad791
Ricardo Moinhos <ricardomo@lsretail.com>
2022-03-24 21:22:47 +0000

    Testing changes.

--- SetupHybridCloudServer.ps1
+++ SetupHybridCloudServer.ps1
@@ -94,9 +94,7 @@ else {
     Import-Module Az.Storage
 
     $licenseFileName = 'DEV.flf'
-    
-    $sasToken = '?sv=2020-08-04&ss=b&srt=o&se=2022-03-24T16%3A42%3A39Z&sp=rl&sig=fENFcm8hXng%2BJUF1QcCnWgzhOL6f%2FtqehEsEHF6ELmY%3D'
-    $storageAccountContext = New-AzStorageContext $StorageAccountName -SasToken $sasToken
+    $storageAccountContext = New-AzStorageContext $StorageAccountName -SasToken $StorageSasToken
 
     $LicenseFileSourcePath = "c:\demo\license.flf"
     $LicenseFileDestinationPath = (Join-Path $HCCProjectDirectory 'Files/License')
