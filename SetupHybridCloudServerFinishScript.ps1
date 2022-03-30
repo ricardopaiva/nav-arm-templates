6e01bf2b6c488b65dd93b275c388dfafd5149c0f
Ricardo Moinhos <ricardomo@lsretail.com>
2022-03-30 10:53:49 +0100

    Testing changes.
    Added missing AddToStatus function.

--- SetupHybridCloudServerFinishScript.ps1
+++ SetupHybridCloudServerFinishScript.ps1
@@ -1,3 +1,10 @@
+if (!(Test-Path function:AddToStatus)) {
+    function AddToStatus([string]$line, [string]$color = "Gray") {
+        ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortDatePattern) + " " + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" -Force -ErrorAction SilentlyContinue
+        Write-Host -ForegroundColor $color $line 
+    }
+}
+
 . (Join-Path $PSScriptRoot "settings.ps1")
 
 AddToStatus "Finishing the Hybrid Cloud Components installation"
