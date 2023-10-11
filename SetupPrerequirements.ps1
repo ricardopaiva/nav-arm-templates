1be5c6c95930ba6a000ec241638053dc3416e8f2
Ricardo Moinhos <ricardomo@lsretail.com>
2023-10-11 21:49:58 +0100

    Forcing Chocolatey version to 1.4.0 because it depends on .net 4.0+.
    Version 2.0.0 depends on .net 4.8 and we were using Chocolatey to install .net 4.8.

--- SetupPrerequirements.ps1
+++ SetupPrerequirements.ps1
@@ -2,14 +2,16 @@ Import-Module (Join-Path $PSScriptRoot "Helpers.ps1") -Force
 
 # Install Choco
 AddToSTatus "Install Choco"
+$env:chocolateyVersion = '1.4.0.0'
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 
-AddToStatus "Install Edge"
-choco install microsoft-edge
-
+# Installing .NET Framework 4.8
 AddToStatus "Install .net Framework 4.8"
 choco install dotnetfx
+
+AddToStatus "Install Edge"
+choco install microsoft-edge
 
 AddToStatus "Install Azure CLI"
 choco install azure-cli
